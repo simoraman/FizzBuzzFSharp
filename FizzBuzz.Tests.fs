c109f4169dad2e094d6d6b8e3d2e1eb86b369f8a
Simo Raman <simoraman@gmail.com>
2012-09-07 07:32:39 +0300

    FizzBuzz in F#

---
+++ FizzBuzz.Tests.fs
@@ -0,0 +1,37 @@
+﻿open FizzBuzzer
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+type ``When to Fizz`` ()=
+    [<Test>] 
+    member test.``One should not fizzbuzz``() = 
+        FizzBuzz 1 |> should equal "1"
+
+    [<Test>] 
+    member test.``3 should fizz``() = 
+        FizzBuzz 3 |> should equal "fizz"
+
+    [<Test>] 
+    member test.``5 should buzz``() = 
+        FizzBuzz 5 |> should equal "buzz"
+
+    [<TestCaseAttribute 3>]
+    [<TestCaseAttribute 6>]
+    [<TestCaseAttribute 9>]
+    [<TestCaseAttribute 12>]  
+    member test.``multiplies of 3 should fizz``(x) = 
+        FizzBuzz x |> should equal "fizz"
+
+    [<TestCaseAttribute 5>]
+    [<TestCaseAttribute 10>]
+    [<TestCaseAttribute 20>]  
+    member test.``multiplies of 5 should buzz``(x) = 
+        FizzBuzz x |> should equal "buzz"
+
+    [<TestCaseAttribute 15>]
+    [<TestCaseAttribute 30>]
+    member test.``multiplies of 3 and 5 should fizzbuzz``(x) = 
+        FizzBuzz x |> should equal "fizzbuzz"
+
+
---
+++ FizzBuzz.fs
@@ -0,0 +1,11 @@
+﻿module FizzBuzzer
+
+type System.Int32 with 
+    member x.is_multiply_of number = x % number = 0
+    
+let FizzBuzz (x:int) = 
+    if x.is_multiply_of 3 && x.is_multiply_of 5 then "fizzbuzz"
+    elif x.is_multiply_of 3 then "fizz"
+    elif x.is_multiply_of 5 then "buzz"
+    else x.ToString()
+    
